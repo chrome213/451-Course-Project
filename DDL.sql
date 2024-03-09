fb203b7baf461e3a0ff91bb3262959e1a2087e5e
Joshua Burk <joshua.burk@wsu.edu>
2024-03-08 19:42:44 -0800

    Added some SQL statements.

--- DDL.sql
+++ DDL.sql
@@ -3,3 +3,44 @@ CREATE TABLE zipcode (
   avg_income INT NOT NULL,
   population INT NOT NULL
 );
+
+CREATE TABLE Business (
+    business_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    zipcode INT,
+    address VARCHAR(255),
+    review_count INT,
+    state VARCHAR(255),
+    average_rating DECIMAL,
+    total_checkins INT,
+    stars DECIMAL,
+    FOREIGN KEY (zipcode) REFERENCES Zipcode(zipcode_id)
+);
+
+CREATE TABLE Review (
+    review_id INT PRIMARY KEY,
+    stars DECIMAL,
+    business_id INT,
+    FOREIGN KEY (business_id) REFERENCES Business(business_id)
+);
+
+CREATE TABLE Categories (
+    name VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE Given (
+    business_id INT,
+    category_id INT,
+    PRIMARY KEY (business_id, name),
+    FOREIGN KEY (business_id) REFERENCES Business(business_id),
+    FOREIGN KEY (name) REFERENCES Categories(name)
+);
+
+CREATE TABLE Check_ins (
+    business_id INT,
+    day DATE,
+    count INT,
+    PRIMARY KEY (business_id, day),
+    FOREIGN KEY (business_id) REFERENCES Business(business_id)
+);
