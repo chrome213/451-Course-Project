3737b349274c9beae7a8fd0dcb51a566163ca59c
Joshua Burk <joshua.burk@wsu.edu>
2024-04-06 03:50:29 -0700

    modified update format

--- Data_Bros_UPDATE.sql
+++ Data_Bros_UPDATE.sql
@@ -1,17 +1,23 @@
 -- Updates the buisness table with the number of check-ins for each business
 UPDATE Business 
-SET numCheckins = (SELECT SUM(count) 
-FROM Check_ins 
-WHERE Check_ins.business_id = Business.business_id);
+SET numCheckins = (
+    SELECT SUM(count) 
+    FROM Check_ins 
+    WHERE Check_ins.business_id = Business.business_id
+);
 
 -- Updates the business table with the number of reviews for each business
 UPDATE Business 
-SET review_count = (SELECT COUNT(*) 
-FROM Review 
-WHERE Review.business_id = Business.business_id);
+SET review_count = (
+    SELECT COUNT(*) 
+    FROM Review 
+    WHERE Review.business_id = Business.business_id
+);
 
 -- Updates the business table with the average rating for each business
 UPDATE Business 
-SET reviewRating = (SELECT AVG(stars) 
-FROM Review 
-WHERE Review.business_id = Business.business_id);
+SET reviewRating = (
+    SELECT AVG(stars) 
+    FROM Review 
+    WHERE Review.business_id = Business.business_id
+);
