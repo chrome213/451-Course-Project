78045963f1fa260d435c7b8983cea62ec6cf99d8
Joshua Burk <joshua.burk@wsu.edu>
2024-04-19 18:25:47 -0700

    Finished Milestone 3

--- main.py
+++ main.py
@@ -28,9 +28,11 @@ class myApp(QMainWindow):  # Class for the main window
         self.ui.State.currentIndexChanged.connect(self.m2LoadCities) # Load cities for the selected state using the selectLocation table
         self.ui.City.itemClicked.connect(self.m2LoadZipcode)
         self.ui.Zipcode.itemClicked.connect(self.m2ZipcodeStatistics)
-        self.ui.Search.clicked.connect(self.m2Filter)
-        self.ui.SelectCategory.itemClicked.connect(self.m2Businesses)
-        self.ui.SelectCategory.itemClicked.connect(self.fetchBusinessesWithScores)
+        self.ui.Zipcode.itemClicked.connect(self.m2Filter)
+        self.ui.Search.clicked.connect(self.m2Businesses)
+        self.ui.Clear.clicked.connect(self.clearBusinessTable)
+        self.ui.Refresh.clicked.connect(self.SuccessfulBusinesses)
+        self.ui.Refresh.clicked.connect(self.PopularBusinesses)
 
     # Connects to the database passed in, we have two databases, one for milestone 1 and one for milestone 2
     def connectToDB(self, dbName):
@@ -99,8 +101,41 @@ class myApp(QMainWindow):  # Class for the main window
         except psycopg2.Error as e:
             print(f'Failed to fetch states. Error: {e}')
    
+       # Milestone 2 clear business table function
+    
+    # clear the business table
+    def clearBusinessTable(self):
+        self.ui.Businesses.clearContents()
+        self.ui.Businesses.setRowCount(0)
+        self.ui.Businesses.setColumnCount(0)
+
+    # clear the category table
+    def clearCategoryTable(self):
+        self.ui.TopCategories.clearContents()
+        self.ui.TopCategories.setRowCount(0)
+        self.ui.TopCategories.setColumnCount(0)
+
+    # clear the successful and popular tables
+    def clearSuccessfulPopularTables(self):
+        self.ui.Successful.clearContents()
+        self.ui.Successful.setRowCount(0)
+        self.ui.Successful.setColumnCount(0)
+        self.ui.Popular.clearContents()
+        self.ui.Popular.setRowCount(0)
+        self.ui.Popular.setColumnCount(0)
+
    # Milestone 2 load cities function that populates the milestone city combobox
     def m2LoadCities(self):
+        self.ui.City.clear()
+        self.ui.Zipcode.clear()
+        self.ui.BusinessesStatistic.clear()
+        self.ui.PopulationStatistic.clear()
+        self.ui.IncomeStatistic.clear()
+        self.clearCategoryTable()
+        self.ui.SelectCategory.clear()
+        self.clearBusinessTable()
+        self.clearSuccessfulPopularTables()
+
         self.connectToDB("milestone2db")
         selected_state = self.ui.State.currentText()   
         self.ui.City.clear()  
@@ -114,6 +149,15 @@ class myApp(QMainWindow):  # Class for the main window
 
     # Milestone 2 load zipcodes function that populates the milestone zipcode combobox
     def m2LoadZipcode(self):
+        self.ui.Zipcode.clear()
+        self.ui.BusinessesStatistic.clear()
+        self.ui.PopulationStatistic.clear()
+        self.ui.IncomeStatistic.clear()
+        self.clearCategoryTable()
+        self.ui.SelectCategory.clear()
+        self.clearBusinessTable()
+        self.clearSuccessfulPopularTables()
+
         self.connectToDB("milestone2db")
         selected_city = self.ui.City.currentItem().text() # Get the selected city
         self.ui.Zipcode.clear()  
@@ -127,6 +171,14 @@ class myApp(QMainWindow):  # Class for the main window
 
     # Milestone 2 load statistics function that populates the milestone statistics Label and Top Categories
     def m2ZipcodeStatistics(self):
+        self.ui.BusinessesStatistic.clear()
+        self.ui.PopulationStatistic.clear()
+        self.ui.IncomeStatistic.clear()
+        self.clearCategoryTable()
+        self.ui.SelectCategory.clear()
+        self.clearBusinessTable()
+        self.clearSuccessfulPopularTables()
+
         self.connectToDB("milestone2db")
         selected_zipcode = self.ui.Zipcode.currentItem().text()  # Get the selected zipcode
 
@@ -180,8 +232,11 @@ class myApp(QMainWindow):  # Class for the main window
         self.cur.close()
         self.conn.close()
 
-    # Milestone 2 filter function that populates the milestone category dearch combobox
+    # Milestone 2 filter function that populates the milestone category search combobox
     def m2Filter(self):
+        # check to see if the user has selected a zipcode
+        if self.ui.Zipcode.currentItem() is None:
+            return
         self.connectToDB("milestone2db")
         selected_zipcode = self.ui.Zipcode.currentItem().text()
         self.ui.SelectCategory.clear()
@@ -204,6 +259,9 @@ class myApp(QMainWindow):  # Class for the main window
 
     # Milestone 2 businesses function that populates the milestone business table
     def m2Businesses(self):
+        # make sure that the user has selected a zipcode and category
+        if self.ui.Zipcode.currentItem() is None or self.ui.SelectCategory.currentItem() is None:
+            return
         self.connectToDB("milestone2db")
         selected_zipcode = self.ui.Zipcode.currentItem().text()
         selected_category = self.ui.SelectCategory.currentItem().text()
@@ -246,38 +304,89 @@ class myApp(QMainWindow):  # Class for the main window
         
         self.cur.close()
         self.conn.close()
-    
-    # Milestone 2 fetch businesses with scores function that populates the milestone business table
-    def fetchBusinessesWithScores(self):
+
+    def SuccessfulBusinesses(self):
+        # make sure that the user has selected a zipcode and category
+        if self.ui.Zipcode.currentItem() is None or self.ui.SelectCategory.currentItem() is None:
+            return
         self.connectToDB("milestone2db")
         selected_zipcode = self.ui.Zipcode.currentItem().text()
         selected_category = self.ui.SelectCategory.currentItem().text()
+        self.ui.Successful.clearContents()
+        self.ui.Successful.setColumnCount(4)
+        self.ui.Successful.setHorizontalHeaderLabels(['Business Name', 'Stars', '# of Checkins', '# of Reviews'])
+        header = self.ui.Successful.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+
         try:
             # Query to calculate scores based on provided weights and fetch business details
             self.cur.execute("""
-                SELECT b.name, b.address, b.city, b.stars, b.review_count, b.numCheckins,
+                SELECT b.name, b.address, b.city, b.stars, b.review_count,b.reviewRating, b.numCheckins,
                     (b.numCheckins) AS popularity_score, 
                     ((b.numCheckins * 0.1) + (b.stars * 0.6) + (b.review_count * 0.3)) AS success_score
                 FROM Business AS b
                 JOIN Has AS h ON b.business_id = h.business_id
                 JOIN Categories AS c ON h.category_id = c.name
                 WHERE b.zipcode = %s AND c.name = %s
-                ORDER BY success_score DESC, popularity_score DESC;
+                ORDER BY success_score DESC;
             """, (selected_zipcode, selected_category))
             businesses = self.cur.fetchall()
-            
-            # Print results to the console
-            # print(f"Businesses in zipcode {selected_zipcode} and category '{selected_category}':")
-            # for business in businesses:
-            #     print(f"Name: {business[0]}, Address: {business[1]}, City: {business[2]}, Stars: {business[3]}, Review Count: {business[4]}, Total Checkins: {business[5]}, Popularity Score: {business[6]}, Success Score: {business[7]}")
+            self.ui.Successful.setRowCount(len(businesses))
+            self.ui.Successful.verticalHeader().setVisible(False)
 
+            for row, (name, address, city, stars, review_count, reviewRating, numCheckins, popularity_score, success_score) in enumerate(businesses):
+                self.ui.Successful.setItem(row, 0, QTableWidgetItem(str(name)))
+                self.ui.Successful.setItem(row, 1, QTableWidgetItem(str(stars)))
+                self.ui.Successful.setItem(row, 2, QTableWidgetItem(str(numCheckins)))
+                self.ui.Successful.setItem(row, 3, QTableWidgetItem(str(review_count)))
+            
         except psycopg2.Error as e:
             print(f'Failed to fetch businesses with scores. Error: {e}')
-        finally:
-            self.cur.close()
-            self.conn.close()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.ResizeToContents)
+        self.cur.close()
+        self.conn.close()
 
+    def PopularBusinesses(self):
+        # make sure that the user has selected a zipcode and category
+        if self.ui.Zipcode.currentItem() is None or self.ui.SelectCategory.currentItem() is None:
+            return
+        self.connectToDB("milestone2db")
+        selected_zipcode = self.ui.Zipcode.currentItem().text()
+        selected_category = self.ui.SelectCategory.currentItem().text()
+        self.ui.Popular.clearContents()
+        self.ui.Popular.setColumnCount(2)
+        self.ui.Popular.setHorizontalHeaderLabels(['Business Name', '# of Checkins'])
+        header = self.ui.Popular.horizontalHeader()
 
+        try:
+            # Query to calculate scores based on provided weights and fetch business details
+            self.cur.execute("""
+                SELECT b.name, b.address, b.city, b.stars, b.review_count, b.numCheckins,
+                    (b.numCheckins) AS popularity_score 
+                FROM Business AS b
+                JOIN Has AS h ON b.business_id = h.business_id
+                JOIN Categories AS c ON h.category_id = c.name
+                WHERE b.zipcode = %s AND c.name = %s
+                ORDER BY popularity_score DESC;
+            """, (selected_zipcode, selected_category))
+            businesses = self.cur.fetchall()
+            self.ui.Popular.setRowCount(len(businesses))
+            self.ui.Popular.verticalHeader().setVisible(False)
+
+            for row, (name, address, city, stars, review_count, numCheckins, popularity_score) in enumerate(businesses):
+                self.ui.Popular.setItem(row, 0, QTableWidgetItem(str(name)))
+                self.ui.Popular.setItem(row, 1, QTableWidgetItem(str(numCheckins)))
+           
+        except psycopg2.Error as e:
+            print(f'Failed to fetch businesses with scores. Error: {e}')
+        # make the first collumn fit the size of the rest of the table
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        self.cur.close()
+        self.conn.close()
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = myApp()
